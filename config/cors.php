7cd2445cb952b3b5de7fcbd95d54fda7c8a9ccba
ikeogu <ikeogu322@gmail.com>
2025-08-23 19:16:59 +0100

    chores: added exchange rate api

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,15 @@ return [
     |
     */
 
-    'paths' => ['*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        '*',
+        'http://localhost:3000',
+        'https://techativedera.com',
+    ],
 
     'allowed_origins_patterns' => [],
 
