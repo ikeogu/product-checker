71a8552a08a48d631d19de5f607309e325e61b00
ikeogu <ikeogu322@gmail.com>
2025-08-23 19:13:19 +0100

    chores: added exchange rate api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,72 +19,72 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-    $middleware->validateCsrfTokens(except: [
-        'http://localhost:8000/*',
-        'http://localhost/*',
-        'http://127.0.0.1:8000/*',
-        'http://127.0.0.1/*',
-        'http://localhost:3000/*',
-        'http://localhost/*',
-        'http://127.0.0.1:3000/*',
-        'http://127.0.0.1/*',
-        'https://detective-omega.vercel.app/*'
+
+        $middleware->validateCsrfTokens(except: [
+            'http://localhost:8000/*',
+            'http://localhost/*',
+            'http://127.0.0.1:8000/*',
+            'http://127.0.0.1/*',
+            'http://localhost:3000/*',
+            'http://localhost/*',
+            'http://127.0.0.1:3000/*',
+            'http://127.0.0.1/*',
+            'https://detective-omega.vercel.app/*'
 
 
-    ]);
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-    $exceptions->render(function (Throwable $e, Request $request) {
-        $wantsJson = $request->is('v1/*') || $request->expectsJson();
+        $exceptions->render(function (Throwable $e, Request $request) {
+            $wantsJson = $request->is('v1/*') || $request->expectsJson();
 
-        if (!$wantsJson) {
-            return null;
-        }
+            if (!$wantsJson) {
+                return null;
+            }
 
-        return match (true) {
-            $e instanceof AuthenticationException => response()->json([
-                'status' => 'failed',
-                'message' => 'Unauthenticated. Please login to proceed'
-            ], Response::HTTP_UNAUTHORIZED),
+            return match (true) {
+                $e instanceof AuthenticationException => response()->json([
+                    'status' => 'failed',
+                    'message' => 'Unauthenticated. Please login to proceed'
+                ], Response::HTTP_UNAUTHORIZED),
 
-            $e instanceof AuthorizationException,
-            $e instanceof AccessDeniedHttpException => response()->json([
-                'status' => 'failed',
-                'message' => $e->getMessage(),
-            ], Response::HTTP_FORBIDDEN),
+                $e instanceof AuthorizationException,
+                $e instanceof AccessDeniedHttpException => response()->json([
+                    'status' => 'failed',
+                    'message' => $e->getMessage(),
+                ], Response::HTTP_FORBIDDEN),
 
-            $e instanceof ModelNotFoundException,
-            $e instanceof NotFoundHttpException => response()->json([
-                'status' => 'failed',
-                'message' => 'Requested resource is not found'
-            ], Response::HTTP_NOT_FOUND),
+                $e instanceof ModelNotFoundException,
+                $e instanceof NotFoundHttpException => response()->json([
+                    'status' => 'failed',
+                    'message' => 'Requested resource is not found'
+                ], Response::HTTP_NOT_FOUND),
 
-            $e instanceof ValidationException => response()->json([
-                'status' => 'failed',
-                'errors' => $e->validator->errors()->all()
-            ], Response::HTTP_UNPROCESSABLE_ENTITY),
+                $e instanceof ValidationException => response()->json([
+                    'status' => 'failed',
+                    'errors' => $e->validator->errors()->all()
+                ], Response::HTTP_UNPROCESSABLE_ENTITY),
 
-            $e instanceof ThrottleRequestsException => response()->json([
-                'status' => 'failed',
-                'message' => $e->getMessage()
-            ], Response::HTTP_TOO_MANY_REQUESTS),
+                $e instanceof ThrottleRequestsException => response()->json([
+                    'status' => 'failed',
+                    'message' => $e->getMessage()
+                ], Response::HTTP_TOO_MANY_REQUESTS),
 
-            $e instanceof TokenMismatchException => response()->json([
-                'status' => 'failed',
-                'message' => $e->getMessage()
-            ], Response::HTTP_FORBIDDEN),
+                $e instanceof TokenMismatchException => response()->json([
+                    'status' => 'failed',
+                    'message' => $e->getMessage()
+                ], Response::HTTP_FORBIDDEN),
 
-            default => response()->json([
-                'status' => 'failed',
-                'message' => $e->getMessage()
-            ], 500),
-        };
-    });
-
+                default => response()->json([
+                    'status' => 'failed',
+                    'message' => $e->getMessage()
+                ], 500),
+            };
+        });
     })->create();
